fe2b350be65316a37059c0190243cc0669de491d
vdalx <114463776+vdalx@users.noreply.github.com>
2022-11-17 22:57:02 -0500

    Made adjustments to JS and HTML for additional weather elements

--- src/app.js
+++ src/app.js
@@ -2,9 +2,13 @@ function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city")
     let descriptionElement = document.querySelector("#description")
+    let humidityElement = document.querySelector("#humidity")
+    let windElement = document.querySelector("#wind")
     temperatureElement.innerHTML = Math.round(response.data.temperature.current);
     cityElement.innerHTML = response.data.city;
     descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = Math.round(response.data.temperature.humidity);
+    windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey ="o57fa7t996f351503fc5e6015403efbd";
