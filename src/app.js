71fbb41e85d041eeef6030adc8f8b17377b54bc6
vdalx <114463776+vdalx@users.noreply.github.com>
2022-11-26 12:14:12 -0500

    Integrated API data into forecast display
    Stored response form API inside variable from forecast and instead of looping through the days identified before.
    
    It's now looping through the forecast variable which has an array of 7 items (days) including current day
    
    Using index to limit results shown.
    
    Created function to format timestamp
    
    Rounded temperature results

--- src/app.js
+++ src/app.js
@@ -13,27 +13,38 @@ function formatDate(timestamp) {
 return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+let date = new Date(timestamp * 1000);
+let day = date.getDay();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+return days[day];
+}
 function displayForecast(response) {
-    console.log(response.data.daily);
+    let forecast = response.data.daily;
     let forecastElement = document.querySelector("#forecast");
     let days =["Sat","Sun","Mon","Tues", "Wed"];
     let forecastHTML = `<div class="row">`;
-    days.forEach(function(day) {    
+    forecast.forEach(function(forecastDay, index) {  
+        if (index < 6) {  
     forecastHTML = 
         forecastHTML +
         `
         <div class="col-2">
-            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-date">${formatDay(forecastDay.time)}</div>
             <img
-                src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
+                src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${forecastDay.condition.icon}.png"
                 alt=""
                 width="36"
             />
             <div class="weather-forecast-temperature">
-                <span class="weather-forecast-temperature-max">18°</span>
-                <span class="weather-forecast-temperature-min">12°</span>
+                <span class="weather-forecast-temperature-max">
+                ${Math.round(forecastDay.temperature.maximum)}°
+                </span>
+                <span class="weather-forecast-temperature-min">
+                ${Math.round(forecastDay.temperature.minimum)}°
+                </span>
             </div>
-        </div>`;
+        </div>`;}
     });
     forecastHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML;
