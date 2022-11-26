87948c0168fb634b350688d8e4ba8a2891780e15
vdalx <114463776+vdalx@users.noreply.github.com>
2022-11-26 10:30:37 -0500

    Updated weather forecast section moving HTML component to JS file
    Removed weather forecast HTML and added it to the JS file in order to create a loop with new function called displayForecast

--- src/app.js
+++ src/app.js
@@ -13,6 +13,30 @@ function formatDate(timestamp) {
 return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+    let days =["Sat","Sun","Mon","Tues", "Wed"];
+    let forecastHTML = `<div class="row">`;
+    days.forEach(function(day) {    
+    forecastHTML = 
+        forecastHTML +
+        `
+        <div class="col-2">
+            <div class="weather-forecast-date">${day}</div>
+            <img
+                src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
+                alt=""
+                width="36"
+            />
+            <div class="weather-forecast-temperature">
+                <span class="weather-forecast-temperature-max">18°</span>
+                <span class="weather-forecast-temperature-min">12°</span>
+            </div>
+        </div>`;
+    });
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+}
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
@@ -75,4 +99,5 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemp);
 
 search("Lisbon");
+displayForecast();
 
